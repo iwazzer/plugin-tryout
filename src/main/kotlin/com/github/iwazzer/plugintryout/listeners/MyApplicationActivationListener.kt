31bbc2202f65fa0c52eebf11f3c0497216931cfd
GitHub Action <action@github.com>
2024-09-27 09:33:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/iwazzer/plugintryout/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/iwazzer/plugintryout/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.iwazzer.plugintryout.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
